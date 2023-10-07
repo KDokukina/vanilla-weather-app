9db7c87e9af9fc61ce14274263ee44643f0b0e31
KDokukina <132554479+KDokukina@users.noreply.github.com>
2023-10-07 22:26:33 +0300

    upgrate

---
+++ src/app.js
@@ -0,0 +1,90 @@
+ function formatDate(timestamp){
+let date = new Date(timestamp);
+ let hours = date.getHours();
+  if (hours < 10) {
+hours = "0" + hours;
+ }
+ let minutes = date.getMinutes();
+  if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+let days = [
+  "Sunday", 
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+ let day = days[date.getDay()];
+ return `${day} ${hours}:${minutes}`;
+}
+
+
+
+function displayTemperature (response){
+    console.log(response.data.main);
+ let temperatureElement = document.querySelector("#temperature");
+
+celsiusTemperature = response.data.main.temp;
+
+ temperatureElement.innerHTML = Math.round(celsiusTemperature);
+ let cityElement = document.querySelector("#city");
+cityElement.innerHTML = response.data.name;
+let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML = response.data.weather[0].description;
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = response.data.main.humidity;
+let windElement = document.querySelector("#wind");
+windElement.innerHTML = Math.round(response.data.wind.speed);
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+let iconElement = document.querySelector("#icon");
+iconElement.setAttribute(
+    "src",
+     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+     iconElement.setAttribute("alt",response.data.weather[0].description);
+
+}
+
+function search(city){
+let apiKey = "505f974b4b9fcd65d904e6fdfac22c0a";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+celsiusLink.classList.remove("active");
+fahrenheitLink.classList.add("active");
+
+    let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32;
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+function displayCelsiusTemperature (event){
+    event.preventDefault();
+    let temperatureElement = document.querySelector ("#temperature");
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+
+ let form = document.querySelector("#search-form");
+ form.addEventListener("submit",handleSubmit);
+
+let fahrenheitLink = document.querySelector ("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector ("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kharkiv");
